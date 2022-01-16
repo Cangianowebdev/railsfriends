2f7989f0dd3c66d66c133bac485999eb36241d07
Marc Cangiano <cangianowebdev@gmail.com>
2022-01-16 11:24:59 -0500

    devise test fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  #devise_for :users
+  devise_for :users
   resources :friends
   #root 'home#index'
   root 'friends#index'
   get 'home/about'
-devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  #devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 end
